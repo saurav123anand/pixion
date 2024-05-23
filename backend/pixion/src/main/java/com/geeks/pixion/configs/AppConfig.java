7f042d895c0a4c6640bcdb874acfd64198906099
saurav123anand <123sauravanand@gmail.com>
2024-05-23 13:02:54 +0530

    secrets removed

--- backend/pixion/src/main/java/com/geeks/pixion/configs/AppConfig.java
+++ backend/pixion/src/main/java/com/geeks/pixion/configs/AppConfig.java
@@ -1,33 +1,14 @@
 package com.geeks.pixion.configs;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    @Value("${s3.region}")
-    private String region;
-    @Value("${s3.accessKey}")
-    private String accessKey;
-    @Value("${s3.accessSecret}")
-    private String accessSecret;
-
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
-    }
-
-    @Bean
-    public AmazonS3 amazonS3(){
-        return AmazonS3ClientBuilder.standard().withRegion(region).
-                withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,accessSecret)))
-                .withPathStyleAccessEnabled(true).build();
     }
 }
--- backend/pixion/src/main/java/com/geeks/pixion/services/S3Service.java
+++
@@ -1,9 +0,0 @@
-package com.geeks.pixion.services;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface S3Service {
-    String uploadImage(MultipartFile multipartFile) throws IOException;
-}

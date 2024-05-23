d9cf64e1c53a9fd6c09705e6b489071bafe3a2c6
saurav123anand <123sauravanand@gmail.com>
2024-05-23 13:27:16 +0530

    Revert "secrets removed"
    This reverts commit 7f042d895c0a4c6640bcdb874acfd64198906099.

---
+++ backend/pixion/src/main/java/com/geeks/pixion/services/impl/S3ServiceImpl.java
@@ -0,0 +1,13 @@
+package com.geeks.pixion.services.impl;
+
+import com.geeks.pixion.services.S3Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public class S3ServiceImpl implements S3Service {
+    @Override
+    public String uploadImage(MultipartFile multipartFile) throws IOException {
+
+    }
+}
